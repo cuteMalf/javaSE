9bdfb85fb97982470bf3b182bbe22f84f1ce05d8
cutema <cutemalinfei@outlook.com>
2022-06-30 22:31:31 +0800

    first commit 2022年6月30日

---
+++ src/main/java/chapter00/HelloWorld.java
@@ -0,0 +1,13 @@
+package chapter00;
+
+public class HelloWorld {
+    /**
+     * 在我的新电脑上开始学习编程
+     * 开头是打印Hello World
+     * 祝我编程愉快
+     * @param args 这入口函数的参数
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
